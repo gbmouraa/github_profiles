03acf741fcf901e5aff480ec3879ae2d5c444951
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2023-11-07 20:32:21 -0300

    add getUser e getUserRepos com axios

--- src/main.js
+++ src/main.js
@@ -1,47 +1,42 @@
-const form = document.querySelector('#form')
-const main = document.querySelector('#main')
-const search = document.querySelector('#search')
+const form = document.querySelector("#form");
+const main = document.querySelector("#main");
+const search = document.querySelector("#search");
 
-async function searchUser(user) {
-    try {
-        const response = await fetch('https://api.github.com/users/' + user)
-        if (!response.ok) {
-            throw new Error('User not found')
-        }
-        const data = await response.json()
-        getUserInfos(data)
-        getRepos('https://api.github.com/users/' + user + '/repos?sort=created')
-        showError(false)
-    } catch (e) {
-        showError(true)
-        console.log(e)
-    }
+const api = axios.create({
+  baseURL: "https://api.github.com/users/",
+});
+
+async function getUser(user) {
+  await api.get(user).then((response) => {
+    getUserInfos(response.data);
+    getUserRepos(`${user}/repos?sort=created`);
+  });
 }
 
 function showError(e) {
-    const errorContainer = document.querySelector('.error')
+  const errorContainer = document.querySelector(".error");
 
-    if (e) {
-        errorContainer.classList.remove('js-no-display')
-        document.querySelector('.user-card').style.display = 'none'
-    } else {
-        errorContainer.classList.add('js-no-display')
-    }
+  if (e) {
+    errorContainer.classList.remove("js-no-display");
+    document.querySelector(".user-card").style.display = "none";
+  } else {
+    errorContainer.classList.add("js-no-display");
+  }
 }
 
 function getUserInfos(data) {
-    const avatar = data.avatar_url
-    const name = data.name ? data.name : data.login
-    const followers = data.followers
-    const following = data.following
-    const qtdRepos = data.public_repos
-    const profileUrl = data.html_url
-    const allInfo = { avatar, name, followers, following, qtdRepos, profileUrl }
-    generateUsercard(allInfo)
+  const avatar = data.avatar_url;
+  const name = data.name ? data.name : data.login;
+  const followers = data.followers;
+  const following = data.following;
+  const qtdRepos = data.public_repos;
+  const profileUrl = data.html_url;
+  const allInfo = { avatar, name, followers, following, qtdRepos, profileUrl };
+  generateUsercard(allInfo);
 }
 
 function generateUsercard(user) {
-    const card = `
+  const card = `
     <div class="user-card animate">
             <div class="avatar-container">
                 <img src="${user.avatar}" alt="user image">
@@ -61,42 +56,40 @@ function generateUsercard(user) {
                 </div>
             </div>
         </div>
-    `
-    main.classList.add('animate')
-    main.innerHTML = card
+    `;
+  main.classList.add("animate");
+  main.innerHTML = card;
 }
 
-async function getRepos(data) {
-    const response = await fetch(data)
-    const repos = await response.json()
-    addReposToCard(repos)
+async function getUserRepos(url) {
+  await api.get(url).then((response) => {
+    addReposToCard(response.data);
+  });
 }
 
 function addReposToCard(data) {
-    const reposUl = document.querySelector('.repo-list')
-    const repos = data
+  const reposUl = document.querySelector(".repo-list");
+  const repos = data;
 
-    repos
-        .slice(0, 5)
-        .forEach(repo => {
-            const repoItem = document.createElement('a')
-            repoItem.classList.add('repo-item')
-            repoItem.href = repo.html_url
-            repoItem.target = '_blank'
-            repoItem.innerText = repo.name
-            const li = document.createElement('li')
-            li.appendChild(repoItem)
-            reposUl.appendChild(li)
-        })
+  repos.slice(0, 5).forEach((repo) => {
+    const repoItem = document.createElement("a");
+    repoItem.classList.add("repo-item");
+    repoItem.href = repo.html_url;
+    repoItem.target = "_blank";
+    repoItem.innerText = repo.name;
+    const li = document.createElement("li");
+    li.appendChild(repoItem);
+    reposUl.appendChild(li);
+  });
 }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    main.classList.remove('animate')
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  main.classList.remove("animate");
 
-    if (search.value.trim() === '') return
+  if (search.value.trim() === "") return;
 
-    searchUser(search.value)
-    search.value = ''
-    search.focus()
-})
+  getUser(search.value);
+  search.value = "";
+  search.focus();
+});
